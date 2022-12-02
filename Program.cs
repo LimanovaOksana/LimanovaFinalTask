5e39711c94e5a5c1c7e866e4a22ff0b46ea98874
Oksana <okslar92@yandex.ru>
2022-12-02 20:50:29 +0700

    Add method

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿
 string[] array1 = new string[5] {"Anna", "Den", "Irina", "Kim", "Yn"};
 string[] array2 = new string[array1.Length];
+void MaxLength3(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
